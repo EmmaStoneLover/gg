bec0bb369bbb293ccdc5f0e1f771c65cd1cbc17d
EmmaStoneLover <gugagaha@gmail.com>
2020-12-07 04:15:47 +0300

    Diploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+import dj-database-url
+
+db_from_env = dj-database-url.config()
+DATABASE['default'].update(db_from_env)
 
 LOGIN_REDIRECT_URL = 'account'
 LOGOUT_REDIRECT_URL = 'login'
