b89812530e74a1ad6f140938a3e8ae5f1ca11c88
EmmaStoneLover <gugagaha@gmail.com>
2020-12-09 18:05:36 +0300

    s

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,18 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = 'static'
-# import dj-database-url
-# db_from_env = dj-database-url.config()
-# DATABASE['default'].update(db_from_env)
+
+# STATIC_ROOT = 'staticfiles'
 
 LOGIN_REDIRECT_URL = 'account'
 LOGOUT_REDIRECT_URL = 'login'
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
