6f89d8a55e9d04a0d282be18abd82c43788dc3c7
EmmaStoneLover <gugagaha@gmail.com>
2021-02-15 23:38:45 +0300

    поиск без учета регистра

--- staticfiles/msg/js/msg.js
+++ staticfiles/msg/js/msg.js
@@ -1,29 +1,20 @@
 
-
-var input = new Vue({
-	el: "#app",
+input_vue = new Vue({
+	el: "#msg_title_and_input",
 	delimiters: ['[[', ']]'],
 	data: {
     msg_input: true,
     msg_input_adds: true,
-
 		vue_text: '',
 		user_check: document.getElementById('user_name').name,
 		vue_user: document.getElementById('user_name').name,
 		vue_card_bg_color_choice: 'bg-dark',
 		vue_card_font_color_choice: 'text-white',
 	},
-	methods: {
-		vue_user_func: function() {
-			if (this.user_check === 'Войти') {this.vue_user = ''}
-			else {this.vue_user = this.user_check}
-		}
-	},
 	beforeMount(){
-	 this.vue_user_func()
+	 this.user_check == 'Войти' ? this.vue_user = '' : this.vue_user = this.user_check
 	},
 });
-
 
 var reader = new FileReader();
 reader.onload = function (e) {
@@ -35,16 +26,34 @@ function input_image_onchange(files) {
 	reader.readAsDataURL(file);
 }
 
+// --------------------------------------------------------- Поиск по Новостям --------
+// msg_id = document.getElementsByClassName('msg_text');
+// for (var i = msg_id.length - 1; i >= 0; i--) {
+//   console.log(i)
+  
+// }
 
+// msg_vue = new Vue({
+//   el: "#msg_search_and_msgs",
+//   delimiters: ['[[', ']]'],
+//   data: {
+    
+//   },
+  
+// });
 
-// Поиск по Новостям
 
 function msg_search_find() {
   $('p#msg_not_found').hide();
   $('div.msg_msgs').show();
   search = $("#msg_search_input").val();
-  search_fined = $('*:contains('+search+')').closest("div.msg_msgs");
-  if (search_fined.length > 0 ) {
+  jQuery.expr[":"].Contains = jQuery.expr.createPseudo(function(arg) {
+    return function( elem ) {
+        return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+  });
+  search_fined = $('*:Contains('+search+')').closest("div.msg_msgs");
+  if (search_fined.length) {
     $('div.msg_msgs').hide();
     search_fined.show();
   }
@@ -59,8 +68,8 @@ function msg_search_clear() {
   $("#msg_search_input").val('');
 };
 $(document).keydown( function(e) {
-  if (e.keyCode === 13) { msg_search_find() }
-  if (e.keyCode === 27) { msg_search_clear() }
+  e.keyCode === 13 ? msg_search_find() : false
+  e.keyCode === 27 ? msg_search_clear() : false
 });
 $('#msg_search_input_button_find').on('click', function() { msg_search_find() });
 $('#msg_search_input_button_clear').on('click', function() { msg_search_clear() });
